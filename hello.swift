aec367c950acdd93cff75d236b9065a8cb80428b
Stepan Paholyk <stepanpaholyk@gmail.com>
2016-11-11 18:23:08 +0200

    Add test const and func

--- hello.swift
+++ hello.swift
@@ -3,3 +3,13 @@ import Foundation
 
 print("Hello World!")
 
+let step = 20
+
+// sorry Michael, don't know exelent Swift syntax yet
+func stepByStep(step: int) -> BOOL {
+    if step < 20 {
+        return true
+    }
+}
+
+
